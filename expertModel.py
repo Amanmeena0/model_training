5f2403d7aabae13d2a86b3424f9db4c9aaefb96e
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-01-13 00:41:07 +0530

    update

--- expertModel.py
+++ expertModel.py
@@ -15,7 +15,6 @@ x_test = x_test[...,tf.newaxis].astype("float32")
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1000).batch(32)
 test_ds = tf.data.Dataset.from_tensor_slices((x_test,y_test)).batch(32)
 
-
 class MyModel(Model):
     def __init__(self):
         super().__init__()
@@ -24,30 +23,30 @@ class MyModel(Model):
         self.d1 = Dense(128, activation='relu')
         self.d2 = Dense(10)
 
-        def call(self, x):
-            x = self.conv1(x)
-            x = self.flatten(x)
-            x = self.d1(x)
-            return self.d2(x)
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
     
 model = MyModel()
 
-#chossing the optimizer and loss function for training
+# #chossing the optimizer and loss function for training
 
 loss_object =tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 optimizer = tf.keras.optimizers.Adam()
 
-#selecting the metrices to measure the loss and accuracy of the model. 
-# These metrics accumulate the values over epochs and then print the overall result.
+# #selecting the metrices to measure the loss and accuracy of the model. 
+# # These metrics accumulate the values over epochs and then print the overall result.
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalCrossentropy(name = 'train_accuracy')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name = 'train_accuracy')
 
 test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.SparseCategoricalCrossentropy(name='test_accuracy')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
-#traing of the model
+# #traing of the model
 @tf.function
 def train_step(images, labels):
     with tf.GradientTape() as tape:

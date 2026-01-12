77a9668f0815e73c27f1d73465298fee2de10a6c
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-01-13 00:33:02 +0530

    udpate

---
+++ expertModel.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+#loading data from random place 
+mnist = tf.keras.datasets.mnist
+
+#splitting the text into train and test datasets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train/ 255.0, x_test/255.0
+
+x_train = x_train[...,tf.newaxis].astype("float32")
+x_test = x_test[...,tf.newaxis].astype("float32")
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test,y_test)).batch(32)
+
+
+class MyModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10)
+
+        def call(self, x):
+            x = self.conv1(x)
+            x = self.flatten(x)
+            x = self.d1(x)
+            return self.d2(x)
+    
+model = MyModel()
+
+#chossing the optimizer and loss function for training
+
+loss_object =tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+optimizer = tf.keras.optimizers.Adam()
+
+#selecting the metrices to measure the loss and accuracy of the model. 
+# These metrics accumulate the values over epochs and then print the overall result.
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalCrossentropy(name = 'train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalCrossentropy(name='test_accuracy')
+
+#traing of the model
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        # training=True is only needed if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        predictions = model(images, training= True  )
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+@tf.function
+def test_step(images, lables):
+    #training = false is only needed if there are layers with different 
+    #behavior during the training verus infernce(e.g dropout)
+
+    predictions = model(images, training=False)
+    t_loss = loss_object(lables, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(lables, predictions)
+
+EPOCHS = 5
+
+for epoch in range(EPOCHS):
+    train_loss.reset_state()
+    train_accuracy.reset_state()
+    test_loss.reset_state()
+    test_accuracy.reset_state()
+
+    for images, labels in train_ds:
+        train_step(images, labels)
+    for test_images, test_lables in test_ds:
+        test_step(test_images, test_lables)
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result():0.2f}, '
+        f'Accuracy: {train_accuracy.result() * 100:0.2f}, '
+        f'Test Loss: {test_loss.result():0.2f}, '
+        f'Test Accuracy: {test_accuracy.result() * 100:0.2f}'
+    )
+
+    
